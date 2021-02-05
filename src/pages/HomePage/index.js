05f46379f221d21d21c83830ace180fd7c7296e4
ohatf091108 <78399803+ohatf091108@users.noreply.github.com>
2021-02-05 20:24:30 +0800

    2021/2/5 20:24

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -43,7 +43,7 @@ export default () => {
         </ul>
         {/* <p>
           從原產地的一株咖啡樹，最終成為您手中的一杯咖啡；我們堅持採購並且烘焙最高品質的咖啡，這是我們一直努力的基本原則；最初的十英呎到最後十英呎的珍貴體驗，為咖啡的故事做了最佳的註解，同時也塑造出咖啡家族的獨特風味及口感特性，閱讀咖啡的故事，可以讓您更瞭解咖啡，豐富您的咖啡體驗。
-        </p> */}
+        </p>  */}
         <p>
           From a coffee tree in the place of origin to a cup of coffee in your
           hands; we insist on purchasing and roasting the highest quality
